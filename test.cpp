a9f273a5d961b1adf2ca127debc4b2a8c7e64bbf
Alex Lai <tutai0320@gmail.com>
2020-12-18 12:08:34 -0800

    Fun Calculator

---
+++ test.cpp
@@ -0,0 +1,8 @@
+//
+//  test.cpp
+//  
+//
+//  Created by Alex Lai on 12/13/20.
+//
+
+#include "test.hpp"
---
+++ test.hpp
@@ -0,0 +1,13 @@
+//
+//  test.hpp
+//  
+//
+//  Created by Alex Lai on 12/13/20.
+//
+
+#ifndef test_hpp
+#define test_hpp
+
+#include <stdio.h>
+
+#endif /* test_hpp */
