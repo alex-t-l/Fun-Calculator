84dc7c09ed1e36a2060333953e157fb0d3f5f5cb
Alan <70036749+alan-lai1738@users.noreply.github.com>
2020-12-18 23:14:18 -0800

    test

--- Fun Calculator/Fun Calculator/EquationCalculator.cpp
+++ Fun Calculator/Fun Calculator/EquationCalculator.cpp
@@ -6,3 +6,16 @@
 //
 
 #include "NormalCalculator.h"
+
+double EquationCalculator::Catalan(double n)
+{
+	if (n == 0 || n == 1)
+	{
+		return 1;
+	}
+	int return_value = 0;
+	for (int i = 0; i < n; i++) {
+		return_value += Catalan(i) * Catalan(n - i - 1);
+	}
+	return return_value;
+}
