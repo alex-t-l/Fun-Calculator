3c0ce99cdd1e33079da6ec67ce335369f5159cc2
Steven <jchow446@gmail.com>
2020-12-18 22:34:29 -0800

    test

--- Fun Calculator/Fun Calculator/NormalCalculator.cpp
+++ Fun Calculator/Fun Calculator/NormalCalculator.cpp
@@ -6,3 +6,51 @@
 //
 
 #include "NormalCalculator.h"
+
+double Add(double a, double b)
+{
+	return 0.0;
+}
+
+double Subtract(double a, double b)
+{
+	return 0.0;
+}
+
+double Divide(double a, double b)
+{
+	return 0.0;
+}
+
+double Multiply(double a, double b)
+{
+	return 0.0;
+}
+
+double Power(double a, double b)
+{
+	return 0.0;
+}
+
+double Log(double a, double b)
+{
+	return 0.0;
+}
+
+double Factorial(double a, double b)
+{
+	return 0.0;
+}
+
+double History(double a, double b)
+{
+	return 0.0;
+}
+
+NormalCalculator::NormalCalculator()
+{
+}
+
+NormalCalculator::~NormalCalculator()
+{
+}
--- Fun Calculator/Fun Calculator/NormalCalculator.h
+++ Fun Calculator/Fun Calculator/NormalCalculator.h
@@ -9,29 +9,33 @@
 #define NormalCalculator_h
 
 #include <vector>
+#include <string>
 #include <stdio.h>
-
-#endif /* NormalCalculator_h */
-
+using namespace std;
 //Normal Calculator Functions
+class NormalCalculator
+{
+public:
+	NormalCalculator();
+	~NormalCalculator();
 
-double Add(double a, double b);
+	double Add(double a, double b);
 
-double Subtract(double a, double b);
+	double Subtract(double a, double b);
 
-double Divide(double a, double b);
+	double Divide(double a, double b);
 
-double Multiply(double a, double b);
+	double Multiply(double a, double b);
 
-double Power(double a, double b);
+	double Power(double a, double b);
 
-double Log(double a, double b);
+	double Log(double a, double b);
 
-double Factorial(double a, double b);
+	double Factorial(double a, double b);
 
-double History(double a, double b);
-
+	double PrintHistory(double a, double b);
 private:
-vector<String> previousTransactions;
-
+	vector<string> history_;
+};
+#endif /* NormalCalculator_h */
 
